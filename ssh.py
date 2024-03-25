0d12f92107fc384d942e0e66675a452809e84073
Kame <kame@duck.com>
2024-03-26 07:00:33 +0900

    Formatted code using Black

--- ssh.py
+++ ssh.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 from warnings import filterwarnings
+
 filterwarnings("ignore")
 
 from twisted.internet import reactor, endpoints, defer
@@ -15,6 +16,7 @@ from zope.interface import implementer
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
+
 @implementer(portal.IRealm)
 class SimpleSSHRealm:
     def requestAvatar(self, avatar_id, mind, *interfaces):
@@ -23,57 +25,56 @@ class SimpleSSHRealm:
         else:
             raise Exception("No supported interfaces found.")
 
+
 def getRSAKeys():
-    public_key_path = os.path.join(script_dir, 'id_rsa.pub')
-    private_key_path = os.path.join(script_dir, 'id_rsa')
+    public_key_path = os.path.join(script_dir, "id_rsa.pub")
+    private_key_path = os.path.join(script_dir, "id_rsa")
 
     if not (os.path.exists(public_key_path) and os.path.exists(private_key_path)):
         ssh_key = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048,
-            backend=default_backend())
+            public_exponent=65537, key_size=2048, backend=default_backend()
+        )
 
         public_key = ssh_key.public_key().public_bytes(
-            serialization.Encoding.OpenSSH,
-            serialization.PublicFormat.OpenSSH)
+            serialization.Encoding.OpenSSH, serialization.PublicFormat.OpenSSH
+        )
 
         private_key = ssh_key.private_bytes(
             serialization.Encoding.PEM,
             serialization.PrivateFormat.TraditionalOpenSSL,
-            serialization.NoEncryption())
+            serialization.NoEncryption(),
+        )
 
-        with open(public_key_path, 'wb') as key_file:
+        with open(public_key_path, "wb") as key_file:
             key_file.write(public_key)
 
-        with open(private_key_path, 'wb') as key_file:
+        with open(private_key_path, "wb") as key_file:
             key_file.write(private_key)
     else:
-        with open(public_key_path, 'rb') as key_file:
+        with open(public_key_path, "rb") as key_file:
             public_key = key_file.read()
 
-        with open(private_key_path, 'rb') as key_file:
+        with open(private_key_path, "rb") as key_file:
             private_key = key_file.read()
 
     return public_key, private_key
+
 
 class CustomSSHServerTransport(transport.SSHServerTransport):
     def __init__(self, our_version_string):
         self.ourVersionString = our_version_string.encode()
         transport.SSHServerTransport.__init__(self)
 
+
 class SimpleSSHFactory(factory.SSHFactory):
     def __init__(self, our_version_string):
         self.ourVersionString = our_version_string
 
-    publicKeys = {
-        b'ssh-rsa': keys.Key.fromString(data=getRSAKeys()[0])
-    }
-    privateKeys = {
-        b'ssh-rsa': keys.Key.fromString(data=getRSAKeys()[1])
-    }
+    publicKeys = {b"ssh-rsa": keys.Key.fromString(data=getRSAKeys()[0])}
+    privateKeys = {b"ssh-rsa": keys.Key.fromString(data=getRSAKeys()[1])}
     services = {
-        b'ssh-userauth': userauth.SSHUserAuthServer,
-        b'ssh-connection': connection.SSHConnection
+        b"ssh-userauth": userauth.SSHUserAuthServer,
+        b"ssh-connection": connection.SSHConnection,
     }
 
     def buildProtocol(self, addr):
@@ -82,25 +83,38 @@ class SimpleSSHFactory(factory.SSHFactory):
         t.factory = self
         return t
 
+
 class LoggingPasswordChecker:
     credentialInterfaces = [credentials.IUsernamePassword]
 
     def requestAvatarId(self, creds):
-        log.msg(f"Login attempt - Username: {creds.username}, Password: {creds.password}")
+        log.msg(
+            f"Login attempt - Username: {creds.username}, Password: {creds.password}"
+        )
         return defer.fail(error.UnauthorizedLogin())
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Run a simple SSH honeypot server.')
-    parser.add_argument('--host', type=str, default='0.0.0.0', help='Host to bind the SSH server to.')
-    parser.add_argument('--port', type=int, default=2222, help='Port to bind the SSH server to.')
-    parser.add_argument('--version', type=str, default='SSH-2.0-OpenSSH_7.4', help='Custom SSH version string to display.')
+    parser = argparse.ArgumentParser(description="Run a simple SSH honeypot server.")
+    parser.add_argument(
+        "--host", type=str, default="0.0.0.0", help="Host to bind the SSH server to."
+    )
+    parser.add_argument(
+        "--port", type=int, default=2222, help="Port to bind the SSH server to."
+    )
+    parser.add_argument(
+        "--version",
+        type=str,
+        default="SSH-2.0-OpenSSH_7.4",
+        help="Custom SSH version string to display.",
+    )
     args = parser.parse_args()
 
     LOG_FILE_PATH = os.path.join(script_dir, "ssh_honeypot.log")
     print(f"SSH HONEYPOT ACTIVE ON HOST: {args.host}, PORT: {args.port}")
     print(f"ALL attempts will be logged in: {LOG_FILE_PATH}")
 
-    log_observer = textFileLogObserver(open(LOG_FILE_PATH, 'a'))
+    log_observer = textFileLogObserver(open(LOG_FILE_PATH, "a"))
     log.startLoggingWithObserver(log_observer, setStdout=False)
 
     ssh_factory = SimpleSSHFactory(args.version)
@@ -112,6 +126,7 @@ def main():
     endpoint = endpoints.TCP4ServerEndpoint(reactor, args.port, interface=args.host)
     endpoint.listen(ssh_factory)
     reactor.run()
+
 
 if __name__ == "__main__":
     main()
